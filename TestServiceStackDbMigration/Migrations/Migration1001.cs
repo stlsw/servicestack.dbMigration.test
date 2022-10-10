ad0d19cff1995ea9be4ca9afecf0be67b5e6dcd5
FireFox2022 <bulaide0713@sina.com>
2022-10-10 20:02:21 +0800

    [更新]初始化

---
+++ TestServiceStackDbMigration/Migrations/Migration1001.cs
@@ -0,0 +1,24 @@
+﻿using ServiceStack.DataAnnotations;
+using ServiceStack.OrmLite;
+
+namespace TestServiceStackDbMigration.Migrations
+{
+    [Description("Update MyTable")]
+    public class Migration1001 : MigrationBase
+    {
+        class MyTable
+        {
+            [Index]
+            public string Code { get; set; }
+
+            [RenameColumn("Name")]
+            public string? FullName { get; set; }
+
+            [RemoveColumn]
+            public double ToDelete { get; set; }
+        }
+
+        public override void Up() => Db.Migrate<MyTable>();
+        public override void Down() => Db.Revert<MyTable>();
+    }
+}
