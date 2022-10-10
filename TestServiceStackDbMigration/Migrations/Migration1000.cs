fa4cf36938667fea79a44560326af1c311e952a1
FireFox2022 <bulaide0713@sina.com>
2022-10-10 19:20:01 +0800

    初始化

---
+++ TestServiceStackDbMigration/Migrations/Migration1000.cs
@@ -0,0 +1,27 @@
+﻿using ServiceStack.DataAnnotations;
+using ServiceStack.OrmLite;
+
+namespace TestServiceStackDbMigration.Migrations
+{
+    [Description("Create initial database tables")]
+    public class Migration1000 : MigrationBase
+    {
+        public class MyTable
+        {
+            [AutoIncrement]
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public double ToDelete { get; set; }
+        }
+
+        public override void Up()
+        {
+            Db.CreateTable<MyTable>();
+        }
+
+        public override void Down()
+        {
+            Db.DropTable<MyTable>();
+        }
+    }
+}
